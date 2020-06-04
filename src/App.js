66daac40d6131f75ee5f7a5a3b7152209b57c8bd
Granite Bagas <granitebagas28@gmail.com>
2020-06-04 23:17:25 +0700

    Add 404 page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import React from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
-import "./App.scss";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Resumes from "./pages/Resumes";
-import Portfolios from "./pages/Portfolios";
-import Blogs from "./pages/Blogs";
-import Contact from "./pages/Contact";
-import BlogDetails from "./pages/BlogDetails";
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import './App.scss';
+import Home from './pages/Home';
+import About from './pages/About';
+import Resumes from './pages/Resumes';
+import Portfolios from './pages/Portfolios';
+import Blogs from './pages/Blogs';
+import Contact from './pages/Contact';
+import BlogDetails from './pages/BlogDetails';
+import Error404 from './pages/Error404';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/about" component={About} />
-        <Route path="/resume" component={Resumes} />
-        <Route path="/portfolios" component={Portfolios} />
-        <Route path="/blogs" exact component={Blogs} />
-        <Route path="/blogs/blog-details/:id/:title" component={BlogDetails} />
-        <Route path="/contact" component={Contact} />
+        <Route path='/' exact component={Home} />
+        <Route path='/about' component={About} />
+        <Route path='/resume' component={Resumes} />
+        <Route path='/portfolios' component={Portfolios} />
+        <Route path='/blogs' exact component={Blogs} />
+        <Route path='/blogs/blog-details/:id/:title' component={BlogDetails} />
+        <Route path='/contact' component={Contact} />
+        <Route component={Error404} />
       </Switch>
     </BrowserRouter>
   );
