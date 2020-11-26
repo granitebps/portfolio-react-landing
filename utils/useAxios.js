dc221a786723696afd1f6821360d195c855de869
Granite Bagas <granitebagas28@gmail.com>
2020-11-26 22:09:28 +0700

    :construction: Change API URL

--- utils/useAxios.js
+++ utils/useAxios.js
@@ -2,12 +2,12 @@ import axios from 'axios';
 import { makeUseAxios } from 'axios-hooks';
 
 export const baseAxios = axios.create({
-  baseURL:
-    process.env.NODE_ENV === 'production'
-      ? 'https://api.granitebps.com/api/v1'
-      : 'http://localhost:8000/api/v1',
+  // baseURL:
+  //   process.env.NODE_ENV === 'production'
+  //     ? 'https://api.granitebps.com/api/v1'
+  //     : 'http://localhost:8000/api/v1',
   // baseURL: 'http://localhost:8000/api/v1',
-  // baseURL: 'http://172.105.127.149/api/v1',
+  baseURL: 'http://34.101.213.193/api/v1',
   headers: {
     'content-tpe': 'application/json',
     Accept: 'application/json',
