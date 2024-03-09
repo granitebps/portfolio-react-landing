6c4661df9345f791f6618428429f2879c5f75164
Granite Bagas <granitebagas28@gmail.com>
2024-03-10 00:20:03 +0700

    feat: Add vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -6,11 +6,17 @@ import '../App.scss';
 import Router from 'next/router';
 import NProgress from 'nprogress'; //nprogress module
 import 'nprogress/nprogress.css'; //styles of nprogress
+import { Analytics } from '@vercel/analytics/react';
 
 Router.events.on('routeChangeStart', () => NProgress.start());
 Router.events.on('routeChangeComplete', () => NProgress.done());
 Router.events.on('routeChangeError', () => NProgress.done());
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
